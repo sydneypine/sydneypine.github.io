72a828a3d56e31a31d4c171ec5a56aab34bcd01e
Sydney <goemailsydney@gmail.com>
2025-11-05 19:56:57 -0600

    Add files via upload

--- truisms.video_Pine/script.js
+++ truisms.video_Pine/script.js
@@ -3,21 +3,28 @@ const videoPlayer = document.getElementById("videoPlayer");
 const textBox = document.getElementById("textBox");
 
 // Array of video sources for toggling
-const videoSources = ["videos/nye2021.mp4", "videos/smoothie.mp4"];
+const videoSources = ["videos/nye2021.mp4", "videos/smoothie.mp4", "videos/mammamia.mp4", "videos/vacuum.mp4", "videos/vent.mp4"];
 // Current video index, starting with the first video
 let currentVideoIndex = 0;
 // Array to store the playback time for each video
-const videoTimes = [0, 0];
+const videoTimes = [0, 0, 0, 0, 0];
 
 var isSoundEnabled = false;
-
+function getRandomIndex() {
+let newIndex;
+do {
+newIndex = Math.floor(Math.random() *
+videoSources.length);
+} while (newIndex === currentVideoIndex);
+return newIndex;
+}
 function toggleVideo() {
 	if (isSoundEnabled === false) {
 		videoPlayer.muted = false;
 		isSoundEnabled = true;
 	}
 videoTimes[currentVideoIndex] = videoPlayer.currentTime;
-currentVideoIndex = 1 - currentVideoIndex;
+currentVideoIndex = getRandomIndex();
 videoPlayer.src = videoSources[currentVideoIndex];
 videoPlayer.load();
 videoPlayer.currentTime = videoTimes[currentVideoIndex];
